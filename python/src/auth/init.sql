4102604c249d28847503896a226bd23c56263de8
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-10-29 18:20:08 -0400

    Fixed missing semicolon in init.sql

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -14,7 +14,7 @@ CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
-)
+);
 
 -- Insert user into user table
 -- This user will have access to the Gateway API
