68e60487001830362025152a2b03418c53f2c6bf
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-10-29 18:10:23 -0400

    Created init.sql to intialize the database

---
+++ python/src/auth/init.sql
@@ -0,0 +1,21 @@
+-- Create database user
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+
+-- Create Database
+CREATE DATABASE auth;
+
+-- Grant access to database for the created user
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+-- Create user table in database
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+)
+
+-- Insert user into user table
+-- This user will have access to the Gateway API
+INSERT INTO user (email, password) VALUES ('admin@gmail.com', 'admin123');
